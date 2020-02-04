59266b713169b3cb315714a5079d763dc4b7e8cd
Carlos Eduardo <cadu_medeirossantos2011@hotmail.com>
2020-02-03 23:13:13 -0300

    DepartmentService

--- src/gui/DepartmentViewController.java
+++ src/gui/DepartmentViewController.java
@@ -1,9 +1,12 @@
 package gui;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import application.Main;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -12,8 +15,11 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 import model.entities.Department;
+import model.services.DepartmentService;
 
 public class DepartmentViewController implements Initializable{
+	
+	private DepartmentService service;
 
 	@FXML
 	private TableView<Department> TableViewDepartment;
@@ -27,9 +33,15 @@ public class DepartmentViewController implements Initializable{
 	@FXML
 	private Button btNew;
 	
+	private ObservableList<Department> obsList;
+	
 	@FXML
 	public void onBtNewAction() {
 		System.out.println("onBtNewAction");
+	}
+	
+	public void setDepartmentService(DepartmentService service) {
+		this.service = service;
 	}
 	
 	@Override
@@ -44,6 +56,16 @@ public class DepartmentViewController implements Initializable{
 		Stage stage = (Stage) Main.getMainScene().getWindow();
 		TableViewDepartment.prefHeightProperty().bind(stage.heightProperty());
 		
+	}
+	
+	public void updateTableView() throws IllegalAccessException {
+		if (service == null) {
+			throw new IllegalAccessException("Service was null");
+		}
+		
+		List<Department> list = service.findAll();
+		obsList = FXCollections.observableArrayList(list);
+		TableViewDepartment.setItems(obsList);
 	}
 	
 }
--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -15,6 +15,7 @@ import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.VBox;
+import model.services.DepartmentService;
 
 public class MainViewController implements Initializable{
 
@@ -34,9 +35,37 @@ public class MainViewController implements Initializable{
 	
 	@FXML
 	public void onMenuItemDepartmentAction() {
-		loadView("/gui/DepartmentList.fxml");
+		loadView2("/gui/DepartmentList.fxml");
 	}
 	
+	private synchronized void loadView2(String absoluteName) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			VBox newVBox = loader.load();
+			
+			Scene mainScene = Main.getMainScene();
+			VBox mainVBox = (VBox)((ScrollPane) mainScene.getRoot()).getContent();
+			
+			Node mainMenu = mainVBox.getChildren().get(0);
+			mainVBox.getChildren().clear();
+			mainVBox.getChildren().add(mainMenu);
+			mainVBox.getChildren().addAll(newVBox.getChildren());
+			
+			DepartmentViewController controller = loader.getController();
+			controller.setDepartmentService(new DepartmentService());
+			controller.updateTableView();
+			
+		}
+		catch(IOException e) {
+			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
+		} 
+		catch (IllegalAccessException e) {
+			Alerts.showAlert("Illegal access exception", "Error loading table view", e.getMessage(), AlertType.ERROR);
+			e.printStackTrace();
+		}
+	}
+	
+
 	@FXML
 	public void onMenuItemHelpAction() {
 		loadView("/gui/viewAbout.fxml");
